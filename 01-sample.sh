977b4cb4bf5f7c9908e623f22feba1b6539cb20d
jagadeesh3737 <jagadeeshvarma444@gmail.com>
2023-11-04 19:17:11 +0530

    added

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,14 +1,14 @@
 color="\e[31m"
-
+color2="\e[34m"
 echo jagadeesh varma
 echo -e "${color} jagadeesh varma \e[0m"
-echo -e "${color} varmajv37.k \e[0m"
+echo -e "${color2} varmajv37.k \e[0m"
 echo -e "${color} jagadeeshvarma \e[0m"
-echo -e "${color} dont do that \e[0m"
+echo -e "${color2} dont do that \e[0m"
 echo -e "${color} jaggu bhai \e[0m"
-echo -e "${color} salaar bhai is coming \e[0m"
-echo -e "${color} installing nginx /e[0m"
+echo -e "${color2} salaar bhai is coming \e[0m"
 echo -e "${color} installing nginx \e[0m"
+echo -e "${color2} installing nginx \e[0m"
 echo -e "${color} dont give up \e[0m"
 
 
