eb27d160661f462ba4e8172260235a492ac1a190
jagadeesh3737 <jagadeeshvarma444@gmail.com>
2023-11-13 13:30:40 +0530

    frontend pushing

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,20 +1,20 @@
-color="\e[31m"
-color2="\e[34m"
-echo jagadeesh varma
-echo -e "${color} jagadeesh varma \e[0m"
-echo -e "${color2} varmajv37.k \e[0m"
-echo -e "${color} jagadeeshvarma \e[0m"
-echo -e "${color2} dont do that \e[0m"
-echo -e "${color} jaggu bhai \e[0m"
-echo -e "${color2} salaar bhai is coming \e[0m"
-echo -e "${color} installing nginx \e[0m"
-echo -e "${color2} installing nginx \e[0m"
-echo -e "${color} dont give up \e[0m"
-echo -e "\e[34m jagadeesh varma \e[0m"
+ # color="\e[31m"
+ # color2="\e[34m"
+ # echo jagadeesh varma
+#  echo -e "${color} jagadeesh varma \e[0m"
+  #echo -e "${color2} varmajv37.k \e[0m"
+ # echo -e "${color} jagadeeshvarma \e[0m"
+ # echo -e "${color2} dont do that \e[0m"
+  #echo -e "${color} jaggu bhai \e[0m"
+ # echo -e "${color2} salaar bhai is coming \e[0m"
+ # echo -e "${color} installing nginx \e[0m"
+ # echo -e "${color2} installing nginx \e[0m"
+  #echo -e "${color} dont give up \e[0m"
+ # echo -e "\e[34m jagadeesh varma \e[0m"
 
-ls
-pwd
-mkdir expense
+ # ls
+ # pwd
+  #mkdir expense
 
 #  if [ $? -ne 0 ]; then
  #   useradd expense
